686765766b9a2e2ee35961efa3c395f6ccb1b5f3
Konrad Lother <konrad@lother.net>
2013-09-05 01:51:31 +0200

    does now replace the folders instead of overwriting.

--- ex/copyincludes.sh
+++ ex/copyincludes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is needed
 
-DIRS=$(/bin/find . -type d |grep -v .git |grep -v include)
-for i in $DIRS; do 
-	cp -rp include $i/; 
+DIRS=$(/bin/find ./*/ -type d |grep -v .git |grep -v include )
+for i in $DIRS; do
+		rm -rf $i/include
+		cp -rp include $i/; 
 done
